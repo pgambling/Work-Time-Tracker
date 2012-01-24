e897259c596047d25b521c9f3a5bd669f2c711e3
Philippe Gambling <pgambling@gmail.com>
2012-01-24 06:38:33 -0600

    Added basic interface.

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = {};
+
+app.projects = [
+	'iSyte',
+	'AS Host',
+	'Backoffice'
+];
+
+app.onProjectStart = function () {
+	console.log("Start project " + app.$selectProject.val());
+};
+
+app.onProjectStop = function () {
+	console.log("Stop project " + app.$selectProject.val());	
+};
+
+app.onLunchStart = function () {
+	console.log("Lunch time!");
+};
+
+app.onLunchEnd = function () {
+	console.log("Lunch is over.");
+};
+
+$(function () {
+	app.$selectProject = $('#selectProject');
+	app.projects.forEach(function (projectName) {
+		$('<option />').text(projectName).appendTo(app.$selectProject);
+	});
+
+	$('#btnProjectStart').on('click', app.onProjectStart);
+	$('#btnLunchStart').on('click', app.onLunchStart);
+	$('#btnLunchEnd').on('click', app.onLunchEnd);
+	$('#btnProjectStop').on('click', app.onProjectStop);		
+});
